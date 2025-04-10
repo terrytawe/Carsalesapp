78ebea12cd513f1648798f605e95c567e39c2bc4
terrytawe <ttawemak@gmail.com>
2025-04-10 16:54:26 +0000

    File rearrangement

--- carsalesapp/carsalesapp/settings.py
+++ carsalesapp/carsalesapp/settings.py
@@ -127,11 +127,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'carsalesapp/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets/static')]
 STATIC_ROOT=os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets/media')
 
 
 # Default primary key field type
