b379aafa7547e1ee6b1f77e603a10d86a6aeb18e
terrytawe <ttawemak@gmail.com>
2025-04-17 13:57:03 +0000

    Added service request forms

--- carsalesapp/servicebookings/urls.py
+++ carsalesapp/servicebookings/urls.py
@@ -2,5 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('create-booking', views.bookings_create, name="create-booking")
+    path('create-booking', views.bookings_create, name="create-booking"),
+    path('create-service', views.service_create, name="create-service"),
+    path('manage-service', views.service_manage, name="manage-service")
 ]
--- carsalesapp/servicebookings/views.py
+++ carsalesapp/servicebookings/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def bookings_create(request):
     return render(request, 'servicebookings/booking-create.html')
+
+def service_create(request):
+    return render(request, 'servicebookings/service-create.html')
+
+def service_manage(request):
+    return render(request, 'servicebookings/service-manage.html', {})
