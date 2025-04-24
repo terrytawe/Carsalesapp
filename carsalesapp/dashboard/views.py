b81bb72378105dc97f6ccbf9c134c3c68ab91d69
terrytawe <ttawemak@gmail.com>
2025-04-24 08:36:33 +0200

    Implemented auth based sections.

--- carsalesapp/dashboard/views.py
+++ carsalesapp/dashboard/views.py
@@ -4,10 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 # ────────────────────────────────────────────────────────────────────────────────────────────────
 def customer_view(request):
-    return render(request, 'dashboard/dashboard-customer.html')
+    return render(request, 'dashboard/dashboard.html')
 
 # ────────────────────────────────────────────────────────────────────────────────────────────────
 #
 # ────────────────────────────────────────────────────────────────────────────────────────────────
 def admin_view(request):
-    return render(request, 'dashboard/dashboard-employee.html')
+    return render(request, 'dashboard/dashboard.html')
